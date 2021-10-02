2309b18e64865989d5b374dd3a638c326fcf0ff0
Todd <todd.labo@ucdenver.edu>
2021-10-01 22:22:33 -0600

    Animated Header NavBar Now Working

--- header-intersection-observer.js
+++ header-intersection-observer.js
@@ -2,11 +2,14 @@ const header =  document.querySelector(".secondary-header");
 const headerPrimary = document.querySelector(".nav__bar")
 const sectionOne = document.querySelector(".section-one");
 const headerLogo = document.querySelector(".nav__bar__logo")
-const headerNavLinks = document.querySelectorAll("a.nav-links")
-console.log(headerNavLinks)
-// const links = headerNavLinks.getElementsByTagName("a");
+const headerLinks = document.querySelectorAll(".nav-link")
+console.log(headerLinks.length)
+
+
+
 
 const sectionOneOptions = {
+    rootMargin: "-500px 0px 0px 0px"
     
 
 }
@@ -15,17 +18,26 @@ const sectionOneOptions = {
 const sectionOneObserver = new IntersectionObserver(function(entries, sectionOneObserver) {
     entries.forEach(entry => {
         if(!entry.isIntersecting){
-        console.log(entry.target);
+        // console.log(entry.target);
         header.classList.add("unactive")
-        headerPrimary.style.padding = "1em 3em"
-        headerLogo.style.fontSize = "2rem"
-        //  headerNavLinks[4].style.paddingLeft = "5em";
+         headerPrimary.style.padding = "1em 3em"
+        headerLogo.style.fontSize = "2rem";
+        for(let i = 0; i < headerLinks.length; i++ ) {
+            headerLinks[i].style.paddingLeft = "3em";
+            console.log(i)
+        }
+       
         }
         else {
+            for(let i = 0; i < headerLinks.length; i++ ) {
+                headerLinks[i].style.paddingLeft = "6em";
+                console.log(i)
+            }
+
             header.classList.remove("unactive")
-            headerPrimary.style.padding = "4em 3em";
-            headerLogo.style.fontSize = "3rem";
-             headerNavLinks[4].style.paddingLeft = "0em";
+            headerPrimary.style.padding = "5em 3em 2em 3em";
+            headerLogo.style.fontSize = "2rem";
+        
         }
 
         
